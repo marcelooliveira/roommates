4f13a3becb1db25dcf74ac43ceb229a29f95d53d
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-02-01 00:04:54 -0300

    loki.js database

---
+++ app/pages/api/rooms/index.js
@@ -0,0 +1,29 @@
+import loki from 'lokijs';
+import { data as roomData } from '../../../data/data.js';
+
+export default async (req, res) => {
+  var db = new loki('roommates.db', 
+    {
+      autoload: true,
+      autoloadCallback : loadHandler,
+      autosave: true, 
+      autosaveInterval: 10000 // 10 seconds
+    });
+
+  function loadHandler() {
+    // if database did not exist it will be empty so I will intitialize here
+    var rooms = db.getCollection('rooms');
+
+    if (rooms === null) {
+      rooms = db.addCollection('rooms');
+      try {
+        // roomData.forEach(room => {
+          rooms.insert(roomData);
+          db.saveDatabase();
+        // });
+      } catch (error) {               
+      }
+    }
+    res.status(200).json(rooms.data);
+  }
+};
--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -2,45 +2,54 @@ import React from "react";
 import Layout from "../components/Layout";
 import {Row, Col} from 'react-bootstrap'
 import Card from "react-bootstrap/Card";
-import { data as realEstates } from '../data/data.js'
 import { faHome as fasHome, faBed as fasBed, faBath as fasBath, faCar as fasCar } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { default as NumberFormat } from 'react-number-format';
+import useSWR from 'swr'
 
-const Home = () => (
-  <Layout>
-    <div className="component-container p-4">
-      <div className="center-panel">
-        <Row>
-          {realEstates.map((realEstate) => (
-            <Col id="hits" className="col-xs-12 col-sm-6 col-md-4 p-3">              
-              <Card className="shadow">
-                  <img src={realEstate.pic} className="card-img-top img-estate" />
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
+const Home = () => {
+  const { data, error } = useSWR('/api/rooms', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  return (
+    <Layout>
+      <div className="component-container p-4">
+        <div className="center-panel">
+          <Row>
+            {data.map((room) => (
+              <Col key={room.number} id="hits" className="col-xs-12 col-sm-6 col-md-4 p-3">
+                <Card className="shadow">
+                  <img src={room.pic} className="card-img-top img-estate" />
                   <Card.Body>
                     <h5 className="card-title">
-                      <NumberFormat value={realEstate.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                      <NumberFormat value={room.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
                       <FontAwesomeIcon icon={farHeart} className="text-danger float-right" />
                     </h5>
-                    <Card.Text><h6>{realEstate.address}</h6></Card.Text>
+                    <Card.Text><h6>{room.address}</h6></Card.Text>
                     <Card.Text className="description" title="{realEstate.description}">
                       <h6>
                         <FontAwesomeIcon icon={fasBed} />
-                        <span>&nbsp;{realEstate.bedrooms}&nbsp;</span>
+                        <span>&nbsp;{room.bedrooms}&nbsp;</span>
                         <FontAwesomeIcon icon={fasBath} />
-                        <span>&nbsp;{realEstate.bathrooms}&nbsp;</span>
+                        <span>&nbsp;{room.bathrooms}&nbsp;</span>
                         <FontAwesomeIcon icon={fasCar} />
-                        <span>&nbsp;{realEstate.cars}&nbsp;</span>
+                        <span>&nbsp;{room.cars}&nbsp;</span>
                       </h6>
                     </Card.Text>
                   </Card.Body>
-              </Card>
-          </Col>
-          ))}
-        </Row>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
       </div>
-    </div>
     </Layout>
-);
+  );
+};
 
 export default Home;
