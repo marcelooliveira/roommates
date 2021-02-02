4c0cf527cbd0d79efb26a5154c4956efef194467
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-02-02 18:44:50 -0300

    Update index.js

--- app/pages/api/rooms/index.js
+++ app/pages/api/rooms/index.js
@@ -4,10 +4,14 @@ import lfsa from 'lokijs/src/loki-fs-structured-adapter';
 import { data as roomData } from '../../../data/data.js';
 
 export default async (req, res) => {
+  
+  var db = new loki('roommates.db');
+  res.status(200).json(db.filename);
+  return;
 
   var adapter = new lfsa();
   
-  var db = new loki('roommates.db', 
+  db = new loki('roommates.db', 
   {
     adapter: adapter,
     autoload: true,
@@ -15,9 +19,6 @@ export default async (req, res) => {
     autosave: true, 
     autosaveInterval: 10000 // 10 seconds
   });
-
-  res.status(200).json(db.filename);
-  return;
 
   function loadHandler() {
     // if database did not exist it will be empty so I will intitialize here
