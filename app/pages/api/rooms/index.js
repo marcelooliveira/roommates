36f1387529bf8501f6b9a65a38c0ca78f845545e
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-02-02 12:18:02 -0300

    Update index.js

--- app/pages/api/rooms/index.js
+++ app/pages/api/rooms/index.js
@@ -2,13 +2,16 @@ import loki from 'lokijs';
 import { data as roomData } from '../../../data/data.js';
 
 export default async (req, res) => {
-  var db = new loki('roommates.db', 
-    {
-      autoload: true,
-      autoloadCallback : loadHandler,
-      autosave: true, 
-      autosaveInterval: 10000 // 10 seconds
-    });
+
+  res.status(200).json(roomData);
+
+  // var db = new loki('roommates.db', 
+  //   {
+  //     autoload: true,
+  //     autoloadCallback : loadHandler,
+  //     autosave: true, 
+  //     autosaveInterval: 10000 // 10 seconds
+  //   });
 
   function loadHandler() {
     // if database did not exist it will be empty so I will intitialize here
