e177b5e3c8e7655a1cb81aa9bdfd861620798c41
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-02-02 15:14:36 -0300

    Update index.js

--- app/pages/api/rooms/index.js
+++ app/pages/api/rooms/index.js
@@ -1,44 +1,52 @@
 import loki from 'lokijs';
+import LokiMemoryAdapter from 'lokijs';
+import lfsa from 'lokijs/src/loki-fs-structured-adapter';
 import { data as roomData } from '../../../data/data.js';
 
 export default async (req, res) => {
-  
+
+  var adapter = new lfsa();
   var db = new loki('roommates.db', 
   {
+    adapter: adapter,
     autoload: true,
     autoloadCallback : loadHandler,
     autosave: true, 
     autosaveInterval: 10000 // 10 seconds
   });
 
+  // wait only for the -current- queue to deplete
+  db.throttledSaveDrain(function () {
+    console.log("queue drained");
+  }, { recursiveWait: false } );
   
   function loadHandler() {
     // if database did not exist it will be empty so I will intitialize here
     var rooms = db.getCollection('rooms');
-    res.status(200).json(rooms);
-    return;
+    // res.status(200).json(rooms);
+    // return;
     
-    // if (rooms === null) {
-    //   rooms = db.addCollection('rooms');
-    //   try {
-    //       rooms.insert(roomData);
-    //       db.saveDatabase();
-    //   } catch (error) {
-    //     res.status(200).json(error);
-    //   }
-    // }
-    // res.status(200).json(rooms.data);
+    if (rooms === null) {
+      rooms = db.addCollection('rooms');
+      try {
+          rooms.insert(roomData);
+          // db.saveDatabase();
+      } catch (error) {
+        res.status(200).json(error);
+      }
+    }
+    res.status(200).json(rooms.data);
 
-    res.status(200).json({
-      "number": 1,
-      "price": 124,
-      "address": "4762  Francis Mine",
-      "pic": "/img/01.jpg",
-      "bedrooms": 5,
-      "bathrooms": 4,
-      "cars": 3,
-      "owner": "marcelooliveira"
-    });
+    // res.status(200).json({
+    //   "number": 1,
+    //   "price": 124,
+    //   "address": "4762  Francis Mine",
+    //   "pic": "/img/01.jpg",
+    //   "bedrooms": 5,
+    //   "bathrooms": 4,
+    //   "cars": 3,
+    //   "owner": "marcelooliveira"
+    // });
 
   }
 };
