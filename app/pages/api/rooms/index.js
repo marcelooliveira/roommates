d110469967ca3f71cab7a10a70996dcb6942120a
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-02-02 13:00:33 -0300

    Update index.js

--- app/pages/api/rooms/index.js
+++ app/pages/api/rooms/index.js
@@ -15,17 +15,27 @@ export default async (req, res) => {
     // if database did not exist it will be empty so I will intitialize here
     var rooms = db.getCollection('rooms');
 
-    if (rooms === null) {
-      rooms = db.addCollection('rooms');
-      try {
-        // roomData.forEach(room => {
-          rooms.insert(roomData);
-          db.saveDatabase();
-        // });
-      } catch (error) {
-        res.status(200).json(error);
-      }
-    }
-    res.status(200).json(rooms.data);
+    // if (rooms === null) {
+    //   rooms = db.addCollection('rooms');
+    //   try {
+    //       rooms.insert(roomData);
+    //       db.saveDatabase();
+    //   } catch (error) {
+    //     res.status(200).json(error);
+    //   }
+    // }
+    // res.status(200).json(rooms.data);
+
+    res.status(200).json({
+      "number": 1,
+      "price": 124,
+      "address": "4762  Francis Mine",
+      "pic": "/img/01.jpg",
+      "bedrooms": 5,
+      "bathrooms": 4,
+      "cars": 3,
+      "owner": "marcelooliveira"
+    });
+
   }
 };
