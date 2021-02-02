cb759928c34a1a1dfd8a7788044535eedd5136d9
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-02-02 15:52:58 -0300

    Update index.js

--- app/pages/api/rooms/index.js
+++ app/pages/api/rooms/index.js
@@ -15,38 +15,31 @@ export default async (req, res) => {
     autosaveInterval: 10000 // 10 seconds
   });
 
-  // wait only for the -current- queue to deplete
-  db.throttledSaveDrain(function () {
-    console.log("queue drained");
-  }, { recursiveWait: false } );
-  
   function loadHandler() {
     // if database did not exist it will be empty so I will intitialize here
     var rooms = db.getCollection('rooms');
-    // res.status(200).json(rooms);
-    // return;
     
-    if (rooms === null) {
-      rooms = db.addCollection('rooms');
-      try {
-          rooms.insert(roomData);
-          // db.saveDatabase();
-      } catch (error) {
-        res.status(200).json(error);
-      }
-    }
-    res.status(200).json(rooms.data);
+    // if (rooms === null) {
+    //   rooms = db.addCollection('rooms');
+    //   try {
+    //       rooms.insert(roomData);
+    //       // db.saveDatabase();
+    //   } catch (error) {
+    //     res.status(200).json(error);
+    //   }
+    // }
+    // res.status(200).json(rooms.data);
 
-    // res.status(200).json({
-    //   "number": 1,
-    //   "price": 124,
-    //   "address": "4762  Francis Mine",
-    //   "pic": "/img/01.jpg",
-    //   "bedrooms": 5,
-    //   "bathrooms": 4,
-    //   "cars": 3,
-    //   "owner": "marcelooliveira"
-    // });
+    res.status(200).json({
+      "number": 1,
+      "price": 124,
+      "address": "4762  Francis Mine",
+      "pic": "/img/01.jpg",
+      "bedrooms": 5,
+      "bathrooms": 4,
+      "cars": 3,
+      "owner": "marcelooliveira"
+    });
 
   }
 };
