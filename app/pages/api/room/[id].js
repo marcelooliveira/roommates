f5dd2b420e0d4572a9e46ea9aa651330f8e004e2
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-02-04 16:05:08 -0300

    Update [id].js

--- app/pages/api/room/[id].js
+++ app/pages/api/room/[id].js
@@ -7,18 +7,37 @@ const { publicRuntimeConfig } = getConfig();
 
 export default async (req, res) => {
   
+  switch (req.method) {
+    case 'GET':
+      getRoom(res, req.query['id']);
+      break
+    case 'POST':
+      if (!req.body.videoId) {
+        res.status(400).end() //Bad Request
+        return;
+      }
+      updateRoom(res, req.query['id'], req.body.videoId);
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+      break
+  }
+};
+
+function getRoom(res, roomId) {
   var adapter = new lfsa();
   var db = new loki(publicRuntimeConfig.lokiDatabase,
-  {
-    adapter: adapter,
-    autoload: true,
-    autoloadCallback : loadHandler
-  });
-  
+    {
+      adapter: adapter,
+      autoload: true,
+      autoloadCallback: loadHandler
+    });
+
   function loadHandler() {
-    
+
     var rooms = db.getCollection('rooms');
-    
+
     if (rooms === null) {
       rooms = db.addCollection('rooms');
       rooms.insert(roomData);
@@ -33,4 +52,33 @@ export default async (req, res) => {
 
     res.status(200).json(doc);
   }
-};
+}
+
+function updateRoom(res, roomId, videoId) {
+  var adapter = new lfsa();
+  var db = new loki(publicRuntimeConfig.lokiDatabase,
+  {
+    adapter: adapter,
+    autoload: true,
+    autoloadCallback : loadHandler
+  });
+  
+  function loadHandler() {
+    var rooms = db.getCollection('rooms');
+    if (!rooms) {
+      res.status(500).json('rooms collection not found!');
+      return;
+    }
+
+    let doc = rooms.get(roomId);
+    if (!doc) {
+      res.status(404).end(`roomId ${roomId} Not Found`);
+      return;
+    }
+
+    doc.videoId = videoId;
+    rooms.update(doc);
+    db.saveDatabase();
+    res.status(200).json('videoId ' + videoId + ' updated successfully.');
+  }
+}
